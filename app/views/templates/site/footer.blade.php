a8f62767e51e3c27adb3325568b2020b4ecd00cf
Владимир <vkharseev@gmail.com>
2015-07-30 14:53:18 +0300

    шаблон главной

--- app/views/templates/site/footer.blade.php
+++ app/views/templates/site/footer.blade.php
@@ -6,17 +6,15 @@
 <footer>
     <div class="holder">
         <div class="left">
-            <a href="#" class="logo"><img src="img/logo2.png" alt=""></a>
+            <a href="#" class="logo"><img src="{{ asset(Config::get('site.theme_path')) }}/img/logo2.png" alt=""></a>
             <div class="copy">
-                © 2015, Amway, A-Gen, 2015 <br>
+                © 2015, Amway, A-Gen, 2015<? if (date('Y') > 2015) { echo '-' . date('Y'); } ?><br>
                 Все права защищены.
             </div>
             <a class="mail" href="mailto:info@agen-project.ru">info@agen-project.ru</a>
         </div>
         <div class="right">
-            <a href="http://myxa-digital.com/" class="dev">
-                MYXA digital
-            </a>
+            <a href="http://myxa-digital.com/" class="dev">MYXA digital</a>
         </div>
     </div>
 </footer>
