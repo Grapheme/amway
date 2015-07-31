526bc8687933d9a03033a2cc8a9d91306e898dd2
etomarat <etomarat@gmail.com>
2015-07-31 17:01:26 +0300

    fix

--- app/views/templates/site/scripts.blade.php
+++ app/views/templates/site/scripts.blade.php
@@ -5,7 +5,9 @@
 ?>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="{{ Config::get('site.theme_path') }}/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
-
+<script src="{{ Config::get('site.theme_path') }}/js/vendor/jquery.ui.widget.js"></script>
+<script src="{{ Config::get('site.theme_path') }}/js/vendor/jquery.iframe-transport.js"></script>
+<script src="{{ Config::get('site.theme_path') }}/js/vendor/jquery.fileupload.js"></script>
 <script src="//cdn.jsdelivr.net/jquery.validation/1.14.0/jquery.validate.min.js"></script>
 <script src="{{ Config::get('site.theme_path') }}/js/messages_ru.js"></script>
 <script src="//cdn.jsdelivr.net/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"></script>
