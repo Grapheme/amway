1d6ff9b29481ae60bae29ace2e73d552f21dfc23
etomarat <etomarat@gmail.com>
2015-08-04 18:22:32 +0300

    fix

--- app/views/templates/site/about.blade.php
+++ app/views/templates/site/about.blade.php
@@ -46,7 +46,8 @@ endif;
 @stop
 @section('content')
     <main>
-        <section class="long" style="background-image: url('{{ asset(Config::get('site.theme_path')) }}/img/space-tmp.jpg')">
+        <section class="long video" style="background-image: url('{{ asset(Config::get('site.theme_path')) }}/img/space-tmp.jpg')">
+            <iframe data-src="https://player.vimeo.com/video/6382511?autoplay=1&loop=1&color=ffffff&title=0&byline=0&portrait=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
             <div class="cover"></div>
             <div class="holder">
                 {{ $page->block('first_section') }}
