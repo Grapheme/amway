bea1cd81f9bf804bb4fb54e7e8a7ac6801dd3502
etomarat <etomarat@gmail.com>
2015-08-05 12:16:46 +0300

    fix

--- public/theme/js/main.js
+++ public/theme/js/main.js
@@ -293,8 +293,11 @@ $(function() {
   $('.videoupload').fileupload({
     dataType: 'json',
     done: function (e, data) {
-      //alert('Готово!'); 
+      //alert('Готово!');
       location.href=location.href;
+      setTimeout(function(){
+        window.location.href='';
+      }, 500)
     },
     fail: function (e, data) {
       alert('Ошибка');
