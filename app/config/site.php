ac6d4e73a670c3ce3c19aa77f1869c1f6227e629
Владимир <vkharseev@gmail.com>
2015-08-05 11:38:50 +0300

    Шаблон одной новости (Фоторепортаж)

--- app/config/site.php
+++ app/config/site.php
@@ -30,7 +30,7 @@ return array(
     'galleries_thumb_public_dir' => '/uploads/galleries/thumbs',
 
     'galleries_photo_size'       => -800, # 800 => 800x600 || 600x800 ; -800 => 800x1000 || 1000x800
-    'galleries_thumb_size'       => -200, # 200 => 200x150 || 150x200 ; -200 => 200x300 || 300x200
+    'galleries_thumb_size'       => -75, # 200 => 200x150 || 150x200 ; -200 => 200x300 || 300x200
 
     'galleries_cache_dir'        => public_path('uploads/galleries/cache'),
     'galleries_cache_public_dir' => '/uploads/galleries/cache',
