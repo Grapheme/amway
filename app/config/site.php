eed7a0f2792843b076134caa5c3ce6a9b9a9c76a
Владимир <vkharseev@gmail.com>
2015-08-05 12:44:23 +0300

    difference_final

--- app/config/site.php
+++ app/config/site.php
@@ -3,6 +3,7 @@
 return array(
 
     'date_over_action' => '2015-09-15',
+    'date_final_action' => '2015-11-24',
 
     'theme_path' => URL::to('/theme'),
     #'theme_path' => URL::to('/theme/' . Config::get('app.template') . '/dist'),
