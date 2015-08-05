4abe1ba36ae2a9da6c1618029505e2d6f5e8c241
etomarat <etomarat@gmail.com>
2015-08-05 12:53:31 +0300

    fix

--- app/views/templates/site/footer.blade.php
+++ app/views/templates/site/footer.blade.php
@@ -18,7 +18,7 @@
             <a class="mail" href="mailto:info@agen-project.ru">info@agen-project.ru</a>
         </div>
         <div class="right">
-            <a href="http://myxa-digital.com/" class="dev">MYXA digital</a>
+            <a href="http://myxa-digital.com/" target="_blank" class="dev">MYXA digital</a>
         </div>
     </div>
 </footer>
