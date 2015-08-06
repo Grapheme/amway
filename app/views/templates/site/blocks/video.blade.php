4c24fff27b032764c1ab479f5e752736ace6bbf8
Владимир <vkharseev@gmail.com>
2015-08-06 13:24:33 +0300

    Голосование за участника, начисление рейтинга

--- app/views/templates/site/blocks/video.blade.php
+++ app/views/templates/site/blocks/video.blade.php
@@ -1,5 +1,5 @@
 <a href="javascript:void(0);" data-href="video" data-name="{{ $video->name }}" data-location="{{ $video->location }}"
-   data-user-id="{{ $video->id }}" data-vote-count="{{ count($video->likes) }}"
+   data-user-id="{{ $video->id }}" data-vote-count="{{ count($video->likes) + $video->guest_likes }}"
    data-vote-url="{{ URL::route('participant.public.set.like', $video->id) }}" data-src="{{{ $video->video }}}"
    class="wrapper btn-popup">
     <div class="frame">
@@ -13,8 +13,7 @@
         <div class="location">{{ $video->location }}</div>
         <div class="rating">
             <span class="icon2-star"></span>
-
-            <div class="count">{{ count($video->likes) }}</div>
+            <div class="count">{{ count($video->likes) + $video->guest_likes }}</div>
             <div class="legend">{{ Lang::choice('голос|голоса|голосов', (int)count($video->likes) ) }}</div>
         </div>
     </div>
