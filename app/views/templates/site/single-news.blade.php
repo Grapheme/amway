063e3fd3c082639a3eb86c65e0291a758457e6dc
Andrew G <voliomas.werdna@gmail.com>
2015-08-14 16:49:07 +0300

    participants

--- app/views/templates/site/single-news.blade.php
+++ app/views/templates/site/single-news.blade.php
@@ -37,18 +37,7 @@
                         </div>
                         <div class="text">
                             {{ $news->meta->content }}
-                            <p>Поделись с друзьями:</p>
-                            <script type="text/javascript">(function() {
-                              if (window.pluso)if (typeof window.pluso.start == "function") return;
-                              if (window.ifpluso==undefined) { window.ifpluso = 1;
-                                var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
-                                s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
-                                s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
-                                var h=d[g]('body')[0];
-                                h.appendChild(s);
-                              }})();
-                            </script>
-                            <div class="pluso" data-background="transparent" data-options="big,round,line,horizontal,nocounter,theme=04" data-services="vkontakte,odnoklassniki,facebook,google,email"></div>
+                            <script type="text/javascript" src="//yastatic.net/share/share.js" charset="utf-8"></script><div class="yashare-auto-init" data-yashareL10n="ru" data-yashareType="link" data-yashareQuickServices="vkontakte,facebook,odnoklassniki"></div>
                         </div>
                     </div>
                 @endif
