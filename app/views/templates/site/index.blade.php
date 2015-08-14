3c9a43ebb06a4735f965c2897418dcaf4789c2e0
Andrew G <voliomas.werdna@gmail.com>
2015-08-14 18:36:08 +0300

    msl

--- app/views/templates/site/index.blade.php
+++ app/views/templates/site/index.blade.php
@@ -108,7 +108,7 @@ endif;
                     <div class="title">
                         &nbsp;
                     </div>
-                    <p>Заливай креативное видео, в котором опиши свою идею</p>
+                    <p>Заливай креативное видео, в котором покажи <br>свою идею</p>
                 </div>
                 <div class="unit">
                     <img src="http://agen-project.ru/theme/img/stage-main-6.png" alt="">
