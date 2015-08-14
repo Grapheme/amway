11472467bb789671a046c81cfe8b9231f4589d60
Andrew G <voliomas.werdna@gmail.com>
2015-08-14 16:40:42 +0300

    participants

--- app/views/templates/site/single-news.blade.php
+++ app/views/templates/site/single-news.blade.php
@@ -37,6 +37,18 @@
                         </div>
                         <div class="text">
                             {{ $news->meta->content }}
+                            <br><br>
+                            <script type="text/javascript">(function() {
+                              if (window.pluso)if (typeof window.pluso.start == "function") return;
+                              if (window.ifpluso==undefined) { window.ifpluso = 1;
+                                var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+                                s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+                                s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+                                var h=d[g]('body')[0];
+                                h.appendChild(s);
+                              }})();
+                            </script>
+                            <div class="pluso" data-background="transparent" data-options="big,round,line,horizontal,nocounter,theme=04" data-services="vkontakte,odnoklassniki,facebook,google,email"></div>
                         </div>
                     </div>
                 @endif
