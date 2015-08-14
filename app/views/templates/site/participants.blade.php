b23248c2dc1c7b33290764d2b408fd0f291dd0c3
Andrew G <voliomas.werdna@gmail.com>
2015-08-14 18:44:26 +0300

    msl

--- app/views/templates/site/participants.blade.php
+++ app/views/templates/site/participants.blade.php
@@ -27,7 +27,7 @@ endif;
 <main>
     <section class="long color-blue video"
              style="background-image: url('{{ asset(Config::get('site.theme_path')) }}/img/tmp-visual-13.jpg')">
-        <iframe data-src="https://player.vimeo.com/video/135698166?autoplay=1&loop=1&color=ffffff&title=0&byline=0&portrait=0"
+        <iframe data-src="https://player.vimeo.com/video/136314283?autoplay=1&loop=1&color=ffffff&title=0&byline=0&portrait=0"
                 frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
         <div class="cover"></div>
         <div class="holder">
