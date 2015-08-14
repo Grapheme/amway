db583e14782d07132f458249ede8815abebdadf1
Andrew G <voliomas.werdna@gmail.com>
2015-08-14 16:14:51 +0300

    participants

--- app/views/templates/site/participants.blade.php
+++ app/views/templates/site/participants.blade.php
@@ -26,7 +26,7 @@ endif;
 @section('content')
 <main>
     <section class="long color-blue video"
-             style="background-image: url('{{ asset(Config::get('site.theme_path')) }}/img/tmp-visual-12.jpg')">
+             style="background-image: url('{{ asset(Config::get('site.theme_path')) }}/img/tmp-visual-13.jpg')">
         <iframe data-src="https://player.vimeo.com/video/135698166?autoplay=1&loop=1&color=ffffff&title=0&byline=0&portrait=0"
                 frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
         <div class="cover"></div>
