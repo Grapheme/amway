40cd509107629e8227de54fbc86b5c5092632ab8
Andrew G <voliomas.werdna@gmail.com>
2015-08-14 12:08:19 +0300

    agen

--- app/views/templates/site/news.blade.php
+++ app/views/templates/site/news.blade.php
@@ -46,6 +46,7 @@
         <div class="holder">
             <h3>ГРАФИК ПЕРЕМЕЩЕНИЯ МОБИЛЬНОЙ КОМАНДЫ</h3>
             <img src="/uploads/files/1438690660_1094242.jpg" style="width:100%;" alt="">
+            <p>&nbsp;</p>
         </div>
     </main>
 @stop
