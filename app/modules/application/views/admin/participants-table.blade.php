b1df7a664b7825d68704d07f2bdc3d78b9eaf778
Владимир <vkharseev@gmail.com>
2015-08-18 14:28:56 +0300

    список email адресов всех участников проекта

---
+++ app/modules/application/views/admin/participants-table.blade.php
@@ -0,0 +1,46 @@
+@extends(Helper::acclayout())
+@section('style')
+@stop
+@section('content')
+    @include($module['tpl'].'/menu')
+@if($users->count())
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <table class="table table-striped table-bordered min-table white-bg">
+                <thead>
+                <tr>
+                    <th class="col-lg-4 text-center">{{ ucfirst($field) }}</th>
+                    <th class="col-lg-4 text-center">Фамилия </th>
+                    <th class="col-lg-4 text-center">Имя</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($users as $index => $user)
+                    <tr class="vertical-middle">
+                        <?php $fio = explode(' ', $user->name);?>
+                        <td>{{ $user->$field }}</td>
+                        <td>{{ @$fio[0] }}</td>
+                        <td>{{ @$fio[1] }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@else
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="ajax-notifications custom">
+                <div class="alert alert-transparent">
+                    <h4>Список пуст</h4>
+                    В данном разделе находятся пользователи сайта
+                    <p><br><i class="regular-color-light fa fa-th-list fa-3x"></i></p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endif
+@stop
+@section('scripts')
+@stop
+
