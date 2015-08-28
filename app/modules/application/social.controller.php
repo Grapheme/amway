549beac85621c2b7e2d1d653c9d3297809050cc3
Владимир <vkharseev@gmail.com>
2015-08-28 12:11:32 +0300

    fix Undefined index: city

--- app/modules/application/social.controller.php
+++ app/modules/application/social.controller.php
@@ -58,17 +58,17 @@ class SocialController extends BaseController {
             if ($validate->passes()):
                 return Redirect::to('/#popup=reg')
                     ->with('token', Input::get('token'))
-                    ->with('email', $_user['email'])
-                    ->with('identity', $_user['identity'])
-                    ->with('profile', $_user['profile'])
-                    ->with('first_name', $_user['first_name'])
-                    ->with('last_name', $_user['last_name'])
-                    ->with('city', $_user['city'])
-                    ->with('uid', $_user['uid'])
-                    ->with('photo_big', $_user['photo_big'])
-                    ->with('photo', $_user['photo'])
-                    ->with('network', $_user['network'])
-                    ->with('verified_email', $_user['verified_email']);
+                    ->with('email', @$_user['email'])
+                    ->with('identity', @$_user['identity'])
+                    ->with('profile', @$_user['profile'])
+                    ->with('first_name', @$_user['first_name'])
+                    ->with('last_name', @$_user['last_name'])
+                    ->with('city', @$_user['city'])
+                    ->with('uid', @$_user['uid'])
+                    ->with('photo_big', @$_user['photo_big'])
+                    ->with('photo', @$_user['photo'])
+                    ->with('network', @$_user['network'])
+                    ->with('verified_email', @$_user['verified_email']);
             else:
                 return Redirect::to('/#popup=enter');
             endif;
@@ -79,14 +79,14 @@ class SocialController extends BaseController {
 
         $ulogin = new Ulogin();
         $ulogin->user_id = $userID;
-        $ulogin->network = $_user['network'];
-        $ulogin->identity = $_user['identity'];
+        $ulogin->network = @$_user['network'];
+        $ulogin->identity = @$_user['identity'];
         $ulogin->email = isset($_user['email']) ? $_user['email'] : '';
-        $ulogin->first_name = $_user['first_name'];
-        $ulogin->last_name = $_user['last_name'];
-        $ulogin->photo = $_user['photo'];
-        $ulogin->photo_big = $_user['photo_big'];
-        $ulogin->profile = $_user['profile'];
+        $ulogin->first_name = @$_user['first_name'];
+        $ulogin->last_name = @$_user['last_name'];
+        $ulogin->photo = @$_user['photo'];
+        $ulogin->photo_big = @$_user['photo_big'];
+        $ulogin->profile = @$_user['profile'];
         $ulogin->access_token = isset($_user['access_token']) ? $_user['access_token'] : '';
         $ulogin->country = isset($_user['country']) ? $_user['country'] : '';
         $ulogin->city = isset($_user['city']) ? $_user['city'] : '';
