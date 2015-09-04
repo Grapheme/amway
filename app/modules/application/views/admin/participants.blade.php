f1cd1b4649e5b87b67a70c7522f2c29b4b74d004
Владимир <vkharseev@gmail.com>
2015-09-04 13:49:29 +0300

    отладка видео

--- app/modules/application/views/admin/participants.blade.php
+++ app/modules/application/views/admin/participants.blade.php
@@ -46,7 +46,7 @@
                         @elseif(!$user->load_video)
                             <p>Видео не загружено</p>
                         @elseif($user->load_video && $user->video != '')
-                            <p><a data-content="{{{ '<iframe width="560" height="315" src="'.$user->video.'" frameborder="0" allowfullscreen></iframe>' }}}" data-html="true" data-original-title="Загруженное видео" data-placement="right" rel="popover" class="btn btn-link" href="javascript:void(0);">Смотреть видео</a></p>
+                            <p><a data-content="{{{ '<iframe width="560" height="315" src="'.trim($user->video).'" frameborder="0" allowfullscreen></iframe>' }}}" data-html="true" data-original-title="Загруженное видео" data-placement="right" rel="popover" class="btn btn-link" href="javascript:void(0);">Смотреть видео</a></p>
                         @endif
                             </div>
                         </td>
